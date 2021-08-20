f76582f9897528f224312658969370c52a811be2
carlos <carlose.alvro@gmail.com>
2021-08-20 18:35:18 -0500

    Interfaz Cansat

---
+++ app.py
@@ -0,0 +1,454 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+import functions
+
+PATH_IMGS = r'./Imagenes'
+IMGS = os.listdir(PATH_IMGS)
+IMGS_PATH = [os.path.join(PATH_IMGS,i) for i in IMGS ]
+### DEFINIMOS LOS LIMITES DE HSV respectivamente ##################3
+min_default = [0,0,0]
+max_default = [180,255,255]
+
+##### INICIAMOS LA APP #####
+app = dash.Dash(__name__)
+
+##### AQUI VA TODA LA ESTRUCTURA DE NUESTRA PAGINA 
+app.layout= html.Div(
+  #CONTENEDOR PRINCIPAL
+  className="main-container",
+  children=[
+    # CABEZERA DONDE VA EL LOGO
+    html.Div(
+      className='header', children=[
+        html.Img(src='./assets/Images/Kan-Horizontal.png', className='logo')
+      ]
+    ),
+    # aqui va todo lo demás
+    html.Section(
+      className='main',
+      children=[
+        # iniciamos componente de los tabs (las pestañas para cambiar de métrica a imagen)
+        dcc.Tabs(className='main-tabs',
+                  value='tab-metrica',children=[
+          # iniciamos el primer tab de metrica
+          dcc.Tab(id='tab-metrica',
+                  className='custom-tab',
+                  selected_className='custom-tab--selected', 
+                  label='Métrica', 
+                  value='tab-metrica', children=[
+                    ###Contenido del primer tab
+
+####Este componente interval es muy importante, es el que nos da el tiempo de actualización de la página                    
+                    #ES SU UNÍCO PROPOSITO (ES INVISIBLE, NO SE MOSTRARÁ NADA EN LA PÁGINA)
+                    dcc.Interval(
+                        id='interval-component',
+                        interval=500, # milisegundos
+                        n_intervals=0
+                    ),
+                    # aqui comienza el contenedor de metricas
+                    html.Div(
+                      className='tab-section',
+                      children=[
+                        #TITULO
+                        html.H2('Métrica'),
+                        #CONTENEDOR PRINCIPAL
+                        html.Div(
+                          className='principal-metrica',
+                          children=[
+                            # Sección ALTITUD
+                            html.Div(
+                              className="card-1 altitud",
+                              children = [
+                                html.H4("Altitud"),
+                                html.H3(id='altitud', children="120"),
+                                html.P("metros")
+                              ]
+                            ),
+                            # Sección COORDENADAS
+                            html.Div(
+                              className=("card-2 coordenadas"),
+                              children = [
+                                html.H4("Coordenadas"),
+                                html.Div(
+                                  className="subcard-2 values-2",
+                                  children = [
+                                    html.H3(id='latitud', children="128.5"),
+                                    html.H3(id='longitud', children="-103.9")
+                                  ]
+                                ),
+                                html.Div(
+                                  className= "subcard-2 uds",
+                                  children = [
+                                    html.P("Latitud"),
+                                    html.P("Longitud")
+                                  ]
+                                )
+                              ]
+                            ),
+                            # Sección TEMPERATURA
+                            html.Div(
+                              className="card-1 temperatura",
+                              children = [
+                                html.H4("Temperatura"),
+                                html.H3(id='temperatura', children="30"),
+                                html.P("°C")
+                              ]
+                            ),
+                            # Sección PRESION
+                            html.Div(
+                              className="card-1 presión",
+                              children = [
+                                html.H4("Presión"),
+                                html.H3(id ='presion', children="2"),
+                                html.P("Pa")
+                              ]
+                            ),
+                            # Sección HUMEDAD
+                            html.Div(
+                              className="card-1 humedad",
+                              children = [
+                                html.H4("Humedad"),
+                                html.H3(id='humedad', children="3"),
+                              ]
+                            ),
+                            # Sección ACELEROMETRO
+                            html.Div(
+                              className=("card-3 acelerometro"),
+                              children = [
+                                html.H4("Acelerometro"),
+                                html.Div(
+                                  className="subcard-3 values-3",
+                                  children = [
+                                    html.H3(id ='aceleX', children="128.5"),
+                                    html.H3(id ='aceleY', children="-103.9"),
+                                    html.H3(id ='aceleZ', children="150.0")
+                                  ]
+                                ),
+                                html.Div(
+                                  className= "subcard-3 uds",
+                                  children = [
+                                    html.P("Eje X"),
+                                    html.P("Eje Y"),
+                                    html.P("Eje Z")
+                                  ]
+                                )
+                              ]
+                            ),
+                            # Sección GIROSCOPIO
+                            html.Div(
+                              className=("card-3 giroscopio"),
+                              children = [
+                                html.H4("Giroscopio"),
+                                html.Div(
+                                  className="subcard-3 values-3",
+                                  children = [
+                                    html.H3(id ='giroX', children="128.5"),
+                                    html.H3(id ='giroY', children="-103.9"),
+                                    html.H3(id ='giroZ', children="150.0")
+                                  ]
+                                ),
+                                html.Div(
+                                  className= "subcard-3 uds",
+                                  children = [
+                                    html.P("Eje X"),
+                                    html.P("Eje Y"),
+                                    html.P("Eje Z")
+                                  ]
+                                )
+                              ]
+                            ),
+                            # Sección VELOCIDAD
+                            html.Div(
+                              className="card-1 velocidad",
+                              children = [
+                                html.H4("Velocidad"),
+                                html.H3("30"),
+                                html.P("m/s")
+                              ]
+                            ),
+                            # Sección FECHA
+                            html.Div(
+                              className="card-1 fecha",
+                              children = [
+                                html.H4(id="date", children="20-08-2021"),
+                                html.H4(id="hour", children="17:43"),
+                              ]
+                            ),
+                            # Sección GRAFICA
+                            html.Div(
+                              className="card-graph",
+                              children= [
+                                dcc.Tabs(
+                                  className="tabs-subgraph",
+                                  value="ac-gi-graph",
+                                  children = [
+                                    dcc.Tab( id="ac-gi-graph",
+                                      className="custom-subtab",
+                                      selected_className='custom-subtab--selected',
+                                      label="Graficas",
+                                      value="ac-gi-graph",
+                                      children=[
+                                        html.Div(
+                                          className="graph-ace-giro-container",
+                                          children=[
+                                            dcc.Graph(
+                                              figure=functions.plot_graphs_ace_giro()
+                                            )
+                                          ]
+                                        )
+                                      ]
+                                    ),
+                                    dcc.Tab(id="cansat-graph",
+                                      className="custom-subtab",
+                                      selected_className='custom-subtab--selected',
+                                      label="Cansat",
+                                      value="cansat-graph",
+                                      children=[
+                                          html.Div(
+                                          className="graph-cansat-container",
+                                          children=[
+                                            dcc.Graph(
+                                              figure=functions.plot_cansat()
+                                            )
+                                          ]
+                                        )
+                                      ]
+                                    )
+                                  ]
+                                )
+                              ]
+                            )
+                          ]
+                        )
+                      ]
+                    )
+                  ]),
+          #contenido del segundo tab de imagen 
+          dcc.Tab(id='tab-image',
+                  className='custom-tab', 
+                  selected_className='custom-tab--selected',
+                  label='Imagen', 
+                  value='tab-image', children=[
+                    ###Contenido del segundo tab
+                    ## IMAGEN
+                    html.Div(
+                      className='tab-section',
+                      children=[
+                        # TITULO
+                        html.H2('Imagen'),
+                        # CONTENEDOR PRINCIPAL
+                        html.Div(
+                          className='principal-image',
+                          children=[
+                            #CONTENEDOR GRAFICA
+                            html.Div(
+                              className='graph-container',
+                              children=[
+                                # GRAFICA
+                                html.Div(
+                                  className= "image-container",
+                                  id= "imagen-cansat",
+                                  children = [
+                                    dcc.Graph(
+                                    className='graph',
+                                    figure=functions.plot_image(IMGS_PATH[1], min_default, max_default)
+                                    ),
+                                  ]
+                                ),
+                                # CONTENEDOR BOTONES GRAFICA
+                                html.Div(
+                                  className='graph-buttons',
+                                  children=[
+                                    # BOTON ATRAS
+                                    html.Button(
+                                      'Atras',
+                                      id='boton-atras',
+                                      n_clicks=0,
+                                      n_clicks_timestamp=-1
+                                    ),
+                                    # BOTON ADELANTE
+                                    html.Button(
+                                      'Adelante',
+                                      id='boton-adelante',
+                                      n_clicks=0,
+                                      n_clicks_timestamp=-1
+                                    )
+                                  ]
+                                )
+                              ]
+                            ),
+                            # CONTENEDOR CONTROLADORES
+                            html.Div(
+                              className="controls-container",
+                              children=[
+                                #SLIDERS
+                                html.Div(className='sliders', children=[
+                                  html.H3('Hue'),
+                                  dcc.RangeSlider(
+                                    id='H-slider',
+                                    min = 0,
+                                    max = 180,
+                                    step=1,
+                                    marks = {0:'0',180:'180'},
+                                    value=[33,85],
+                                    allowCross=False,
+                                    updatemode='drag'
+                                  ),
+                                  html.H3('Saturation'),
+                                  dcc.RangeSlider(
+                                    id='S-slider',
+                                    min = 0,
+                                    max = 255,
+                                    step=1,
+                                    marks = {0:'0',255:'255'},
+                                    value=[58,255],
+                                    allowCross=False,
+                                    updatemode='drag'
+                                  ),
+                                  html.H3('Value'),
+                                  dcc.RangeSlider(
+                                    id='V-slider',
+                                    min = 0,
+                                    max = 255,
+                                    step=1,
+                                    marks = {0:'0',255:'255'},
+                                    value=[0,255],
+                                    allowCross=False,
+                                    updatemode='drag'
+                                  )
+                                ]),
+                                # BLURS
+                                html.H3("Desenfoques"),
+                                html.Div(className ='blur-buttons', 
+                                  children=[
+                                    html.Button(
+                                      'Gaussiano',
+                                      id='gauss',
+                                      n_clicks=0,
+                                      n_clicks_timestamp=-1
+                                    ), 
+                                    html.Button(
+                                      'Median',
+                                      id = 'median',
+                                      n_clicks=0,
+                                      n_clicks_timestamp=-1
+                                    )
+                                ]),
+                                # RESET BUTTON
+                                html.Button(
+                                    'Reset',
+                                    id='reset',
+                                    n_clicks=0,
+                                    n_clicks_timestamp=-1
+                                )
+                              ]          
+                            )
+                          ]
+                        )
+                      ]
+                    )
+                  ])
+        ])
+      ]
+    )
+  ]
+)
+
+## ESTA ES LA FUNCIÓN QUE ACTUALIZA LOS VALORES
+## RECIBE AL CONTADOR LLAMADO INTERVAL DE LA LINEA 47
+## EL CONTADOR LE DA LA SEÑAL PARA QUE LOS OUTPUTS SE REFRESQUEN
+## EL INTERVAL TIENE UN TIEMPO DE ACTUALIZACION DE 0.5 SEGUNDOS O 500 MILISEGUNDOS
+@app.callback(
+  dash.dependencies.Output('altitud', 'children'),
+  dash.dependencies.Output('latitud', 'children'),
+  dash.dependencies.Output('longitud', 'children'),
+  dash.dependencies.Output('temperatura', 'children'),
+  dash.dependencies.Output('presion', 'children'),
+  dash.dependencies.Output('humedad', 'children'),
+  dash.dependencies.Output('aceleX', 'children'),
+  dash.dependencies.Output('aceleY', 'children'),
+  dash.dependencies.Output('aceleZ', 'children'),
+  dash.dependencies.Output('giroX', 'children'),
+  dash.dependencies.Output('giroY', 'children'),
+  dash.dependencies.Output('giroZ', 'children'),
+  dash.dependencies.Output('date', 'children'),
+  dash.dependencies.Output('hour', 'children'),
+  [dash.dependencies.Input('interval-component', 'n_intervals')] 
+)
+def data_listener(n):
+  output = functions.open_serial() # ESTA FUNCIÓN TRAE LOS VALORES DEL PUERTO SERIAL
+  al = output[0]
+  la = output[1]
+  lo = output[2]
+  te = output[3]
+  pr = output[4]
+  hu = output[5]
+  ax = output[6]
+  ay = output[7]
+  az = output[8]
+  gx = output[9]
+  gy = output[10]
+  gz = output[11]
+  date = functions.today_date()
+  hour = functions.current_time()
+  return al, la, lo, te, pr, hu, ax, ay, az, gx, gy, gz, date, hour
+
+
+
+## ESTA FUNCIÓN ES LA RESPONSABLE DE LA INTERACCIÓN CON LA IMAGEN 
+## ESTA EN CAMBIO SOLO TIENE 1 OUTPUT QUE ES LA IMAGEN A LA QUE SE LE 
+## PUEDEN MOVER LOS VALORES DE hsv O LOS blurs 
+## LO QUE HACE QUE SE ACTUALICE ES EL CAMBIO EN LO INPUTS
+## QUE EN ESTE CASO SON LOS SLIDERS Y LOS BOTONES
+gau = 0
+med = 0
+res = 0
+
+@app.callback(
+  dash.dependencies.Output("imagen-cansat", "children"),
+  [dash.dependencies.Input('boton-adelante','n_clicks'),
+  dash.dependencies.Input('boton-atras','n_clicks'),
+  dash.dependencies.Input('H-slider','value'),
+  dash.dependencies.Input('S-slider','value'),
+  dash.dependencies.Input('V-slider','value'),
+  dash.dependencies.Input('gauss','n_clicks'),
+  dash.dependencies.Input('median','n_clicks'),
+  dash.dependencies.Input('reset','n_clicks')]
+)
+def image_controler(adelante, atras, H, S, V, gauss, median, reset):
+  global gau, med, res
+
+  image_number = adelante - atras
+  image = IMGS_PATH[image_number]
+  min = [H[0], S[0], V[0]]
+  max = [H[1], S[1], V[1]]
+
+  if gauss!=gau:
+    image = functions.gaussian_blur(image)
+    print("Apply 1 time gauss")
+    gau = gauss
+  else:
+    pass
+
+  if median!=med:
+    image = functions.median_blur(image)
+    print("Apply 1 time median")
+    med =median
+
+  if reset!= res:
+    image = IMGS_PATH[image_number]
+    res = reset
+  else:
+    pass
+  
+  return [dcc.Graph(
+            className='graph',
+            figure= functions.plot_image(image, min, max)
+          ),
+        html.P(f'H: {H}  S: {S}  V: {V}')]
+
+
+if __name__=='__main__':
+  app.run_server(debug=True)
---
+++ functions.py
@@ -0,0 +1,194 @@
+import plotly.express as px
+import numpy as np
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import cv2
+import os 
+import plotly.express as px
+import serial
+import json
+import re
+from datetime import date, datetime
+
+
+
+
+###   FUNCIONES PARA LAS METRICAS ##############
+################################################
+
+## Grafica de lineas acelerometro y giroscopio
+def plot_graphs_ace_giro():
+  x = [i for i in range(1,51)]
+  y1 = np.random.randint(-100,100,50)
+  y2 = np.random.randint(-100,100,50)
+  fig = make_subplots(rows=2, cols=1)
+
+  fig.append_trace(go.Scatter(x=x, y=y1, line=dict(color='#F24333')), row=1, col=1)
+  fig.append_trace(go.Scatter(x=x, y=y2, line=dict(color='#F7B32B')), row=2, col=1)
+
+
+  fig.update_layout(width=550, 
+                    height=330, 
+                    showlegend=False, 
+                    margin=dict(l=20, r=20, t=20, b=20),
+                    paper_bgcolor='rgba(0,0,0,0)',
+                    plot_bgcolor='rgba(0,0,0,0)',
+                    font_color="white")
+  # fig.update_layout(title_text='Acelerometro', title_x=0.5, title_font_size=20)
+  fig.update_yaxes(title_text="Acelerometro", row=1, col=1)
+  fig.update_yaxes(title_text="Giroscopio", row=2, col=1)
+  return fig
+
+## Función para crear las meshgrids del cilindro
+## Dado el radio r, la altura h, y la cordenada de la base a
+def cylinder(r,h, a = 0, nt = 100, nv = 50):
+  theta = np.linspace(0, 2*np.pi, nt)
+  v = np.linspace(a, a+h, nv)
+  theta, v = np.meshgrid(theta,v)
+  x = r*np.cos(theta)
+  y = r*np.sin(theta)
+  z = v
+  return x,y,z
+
+## Grafica para plotear el cilindro "Cansat"
+def plot_cansat():
+  r=2
+  a= -2.5
+  h=5
+  x,y,z = cylinder(r,h,a)
+  colorscale = [[0,'blue'], [1,'blue']]
+
+  X = go.Scatter3d(x=[0,3],y=[0,0], z=[0,0],
+                    showlegend=False,
+                    mode  ='lines',
+                    line = dict(color = 'black', width=2))
+  Y = go.Scatter3d(x=[0,0],y=[0,3], z=[0,0],
+                    showlegend=False,
+                    mode  ='lines',
+                    line = dict(color = 'black', width=2))
+  Z = go.Scatter3d(x=[0,0],y=[0,0], z=[0,3],
+                    showlegend=False,
+                    mode  ='lines',
+                    line = dict(color = 'black', width=2))
+  cyl = go.Surface(x = x, y = y, z = z,
+                  opacity = 0.5,
+                  colorscale=colorscale,
+                  showscale=False)
+
+  fig = go.Figure(data = [cyl,Z,X,Y])
+  fig.update_scenes(xaxis_range=[-5,5],
+                    yaxis_range=[-5,5],
+                    zaxis_range=[-2.5, 2.5])
+  fig.update_layout(width=550, 
+                    height=330, 
+                    showlegend=False, 
+                    margin=dict(l=20, r=20, t=20, b=20),
+                    paper_bgcolor='rgba(0,0,0,0)',
+                    plot_bgcolor='rgba(0,0,0,0)',
+                    font_color="white")
+  return fig
+
+
+
+###   FUNCIONES PARA LAS IMAGENES ##############
+################################################
+
+## Función para cargar la imagen desde el directorio,
+## ya con sus valores por defecto de HSV
+## min y max deben de ser listas de 3 valores 
+## HSV respectivamente para poner los limites.
+## Los valores minimos son 0 
+## Los valores maximos son [180, 255, 255]
+def plot_image(file, min, max):
+  try:
+    img = cv2.imread(file)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  except:
+    img = file
+
+  range1 = np.array(min, np.uint8)
+  range2 = np.array(max, np.uint8)
+
+  imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+  ##Aplicamos la mascara
+  mask = cv2.inRange(imgHSV, range1, range2)
+  segmentacion = cv2.bitwise_and(img, img, mask=mask)
+  fig = px.imshow(segmentacion)
+  fig.update_layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    margin=dict(l=20, r=20, t=20, b=5)
+  )
+  fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+  return fig
+
+
+def median_blur(file):
+  try:
+    img = cv2.imread(file)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  except:
+    img = file
+
+  filtro = cv2.medianBlur(img, 3)
+  return filtro  
+
+def gaussian_blur(file):
+  try:
+    img = cv2.imread(file)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  except:
+    img = file
+
+  filtro = cv2.GaussianBlur(img, (5,5),cv2.BORDER_DEFAULT)
+  return filtro  
+
+
+
+
+# def serial_cleaner():
+#   ### ESTA FUNCIÓN CONVIERTE LO QUE RECIBE DEL PUERTO SERIAL 
+#   ### A UN DICCIONARIO EN PYTHON 
+#   ### SE ESPERA QUE DEL PUERTO SE RECIBA ALGO ASI 
+#   ### ["Al":"201", "La":"119", "Lo":"72", "Te":"15", "Pr":"2", "Hu":"2","Ax":"-2","Ay":"319","Az":"219","Gx":"-136","Gy":"105","Gz":"-349"]
+#   ### NO IMPORTA EL ORDEN SOLO LA ESTRUCTURA
+#   prueba = """["Al":"201", "La":"119", "Lo":"72", "Te":"15", "Pr":"2", "Hu":"2","Ax":"-2","Ay":"319","Az":"219","Gx":"-136","Gy":"105","Gz":"-349"]"""
+#   pattern = r"[\[\{](.+)[\]\}]"
+#   return re.match(pattern, prueba).group(1)
+
+
+def open_serial():
+  ser = serial.Serial('/dev/pts/4', baudrate=115200) ## aqui cambiar el puerto
+  decode_data = ser.readline().decode('utf-8')
+  try:
+    #POR SI LA CADENA VIENE CON CORCHETES Y NO CON LLAVES
+    decode_data = decode_data.replace("[","{").replace("]","}")
+  except:
+    pass
+  dic = json.loads(decode_data)
+  output = [
+    dic['Al'],
+    dic['La'],
+    dic['Lo'],
+    dic['Te'],
+    dic['Pr'],
+    dic['Hu'],
+    dic['Ax'],
+    dic['Ay'],
+    dic['Az'],
+    dic['Gx'],
+    dic['Gy'],
+    dic['Gz'],
+  ]
+  return output
+
+
+
+def today_date():
+  today = date.today()
+  return today.strftime("%d/%m/%Y")
+
+def current_time():
+  now = datetime.now()
+  return now.strftime("%H:%M")
